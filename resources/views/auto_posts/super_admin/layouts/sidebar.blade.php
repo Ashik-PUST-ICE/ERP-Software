1ffefe9e1c65971bdedd5b84b5c789fbfc04d1f2
Ashik-PUST-ICE <ashik.200607@s.pust.ac.bd>
2026-09-25 23:49:50 +0600

    Highlight version update menu

--- resources/views/auto_posts/super_admin/layouts/sidebar.blade.php
+++ resources/views/auto_posts/super_admin/layouts/sidebar.blade.php
@@ -173,7 +173,7 @@
             </li>
 
             <li class="divider"><span>{{ __('System') }}</span></li>
-            <li>
+            <li class="{{ request()->routeIs('ashik.version-update*') || request()->is('erp/super-admin/version-update*') ? 'currrent-menu' : '' }}">
                 <a href="{{ route('ashik.version-update') }}">
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.6663 1.33301V3.99967M5.33301 1.33301V3.99967" stroke="#808080" stroke-width="1.3"
